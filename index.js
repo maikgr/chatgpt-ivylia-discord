f66d0784b434d87a86eefe910e61824426521d36
maikgr <maikgibranr@gmail.com>
2023-04-24 20:26:10 +0800

    Fix attempt to split markdown

--- index.js
+++ index.js
@@ -51,6 +51,107 @@ const sanitizeUsername = (username) => {
   return username.replace(/[^a-zA-Z0-9]/g, '').replace(/\s/g, '').slice(0, 64);
 }
 
+const splitMarkdownString = (str) => {
+  const maxLength = 2000;
+  const lines = str.split('\n');
+  let chunks = [];
+  let currentChunk = '';
+  let isInCodeBlock = false;
+
+  for (let line of lines) {
+    if (line.startsWith('```')) {
+      isInCodeBlock = !isInCodeBlock;
+      if (!isInCodeBlock) {
+        currentChunk += line + '\n';
+        if (currentChunk.length > maxLength) {
+          const chunkLines = currentChunk.split('\n');
+          let chunkLinesLength = 0;
+          let chunkLinesArray = [];
+          for (let i = 0; i < chunkLines.length; i++) {
+            chunkLinesLength += chunkLines[i].length + 1;
+            chunkLinesArray.push(chunkLines[i]);
+            if (chunkLinesLength > maxLength) {
+              chunks.push('```' + chunkLinesArray.join('\n') + '```');
+              chunkLinesArray = [];
+              chunkLinesLength = 0;
+            }
+          }
+          if (chunkLinesArray.length > 0) {
+            chunks.push('```' + chunkLinesArray.join('\n') + '```');
+          }
+          currentChunk = '';
+        }
+      } else {
+        chunks.push(currentChunk);
+        currentChunk = line + '\n';
+      }
+    } else {
+      if (isInCodeBlock) {
+        currentChunk += line + '\n';
+      } else {
+        if (currentChunk.length + line.length + 1 > maxLength) {
+          chunks.push(currentChunk);
+          currentChunk = '';
+        }
+        currentChunk += line + '\n';
+        while (currentChunk.length > maxLength) {
+          const chunk = currentChunk.substring(0, maxLength);
+          chunks.push(chunk);
+          currentChunk = currentChunk.substring(maxLength);
+        }
+      }
+    }
+  }
+
+  if (currentChunk !== '') {
+    chunks.push(currentChunk);
+  }
+
+  const hasMarkdownSyntax = (chunk) => {
+    return chunk.includes('```');
+  };
+
+  const properlyEncloseChunk = (chunk) => {
+    if (hasMarkdownSyntax(chunk)) {
+      return '```\n' + chunk + '```';
+    } else {
+      return chunk;
+    }
+  };
+
+  return chunks.map(properlyEncloseChunk);
+}
+
+const splitRegularString = (str) => {
+  const maxLength = 2000;
+  if (str.length <= maxLength) {
+    return [str];
+  }
+  const chunks = [];
+  let chunkStart = 0;
+  let lastNewlineIndex = -1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '\n') {
+      lastNewlineIndex = i;
+    }
+    if (i - chunkStart >= maxLength) {
+      if (lastNewlineIndex !== -1 && lastNewlineIndex >= chunkStart) {
+        chunks.push(str.slice(chunkStart, lastNewlineIndex));
+        chunkStart = lastNewlineIndex + 1;
+        lastNewlineIndex = -1;
+      } else {
+        chunks.push(str.slice(chunkStart, i));
+        chunkStart = i;
+        lastNewlineIndex = -1;
+      }
+    }
+  }
+  if (chunkStart < str.length) {
+    chunks.push(str.slice(chunkStart));
+  }
+  return chunks;
+}
+
 client.on(Events.MessageCreate, async message => {
   if (message.author.bot || !(await isInWhitelist(message.author.id)) || !message.mentions.has(client.user)) {
     return
@@ -64,14 +165,32 @@ client.on(Events.MessageCreate, async message => {
   message.channel.sendTyping()
   let api = await getAPI(message.channelId)
   const username = process.env.OWNER_ID === message.author.id ? process.env.OWNER_USERNAME : message.member.displayName;
-  const chatGptResponse = await api.sendMessage(messageContent, {
-    name: sanitizeUsername(username) || 'Anonymous',
-  });
+  let responseText = '';
+  try {
+    const chatGptResponse = await api.sendMessage(messageContent, {
+      name: sanitizeUsername(username) || 'Anonymous',
+    });
+    responseText = chatGptResponse.text;
+  }
+  catch (e) {
+    if (e.error.message) {
+      responseText = 'Sorry, unable to generate a response. Please try again later. Error: ```' + e.error.message + '```';
+    }
+    else {
+      throw e;
+    }
+  }
 
   // Discord has a 2000 character limit for messages
-  if (chatGptResponse.text.length > 2000) {
-    console.log("Message too long, splitting into chunks, original response", chatGptResponse);
-    const chunks = chatGptResponse.text.match(/.{1,2000}/g);
+  if (responseText.length > 2000) {
+    let chunks = []
+    if (responseText.includes('```')) {
+      chunks = splitMarkdownString(responseText);
+    }
+    else {
+      chunks = splitRegularString(responseText);
+    }
+
     for (const chunk of chunks) {
       console.log(chunk);
       message.reply({
@@ -84,7 +203,7 @@ client.on(Events.MessageCreate, async message => {
   }
   else {
     message.reply({
-      content: chatGptResponse.text,
+      content: responseText,
       allowedMentions: {
         repliedUser: false,
       },
